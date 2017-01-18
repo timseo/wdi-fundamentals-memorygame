44194170e0f30dbf58881e101152be2cd179588f
timseo27 <timseo@timsmac.local>
2017-01-18 12:36:07 -0800

    created game logic ls

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,1 +1,9 @@
 console.log("JS file is connected to HTML! Woo!")
+
+// added variables to the cards
+
+var cardOne = "queen";
+var cardTwo = "queen";
+var cardThree = "king";
+var cardFour = "king";
+
